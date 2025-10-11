7db2344b5c107a74df3263eeee8e4eb62a1608b7
Hector Kurtyanek <hkurtyanek@adaptavist.com>
2025-10-11 07:49:22 +0100

    add some folders

---
+++ env.d.ts
@@ -0,0 +1,15 @@
+/**
+ * Global Environment Variable Declarations
+ *
+ * This file extends the global ProcessEnv interface to include
+ * type definitions for custom environment variables used in the project.
+ * It ensures type safety and autocompletion when accessing process.env.
+ *
+ */
+
+declare namespace NodeJS {
+  interface ProcessEnv {
+    NODE_ENV?: "production" | "development" | "test";
+    DB_URL: string;
+  }
+}
